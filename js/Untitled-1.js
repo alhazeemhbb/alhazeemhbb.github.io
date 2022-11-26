72ca384baf46a57506a22a8bcfe180872decebbe
alhazeemhbb <106816809+alhazeemhbb@users.noreply.github.com>
2022-11-26 15:54:41 +0700

    Add files via upload

---
+++ js/Untitled-1.js
@@ -0,0 +1,5 @@
+const celsius = prompt("Enter a celsius value: ");
+const fahrenheit = (celsius * 1.8) + 32
+
+
+document.writeln(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
